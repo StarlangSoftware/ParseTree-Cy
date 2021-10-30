7431b7a4589962a55401490c1765ef92e99adcc7
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 22:09:31 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["ParseTree/*.pyx", "ParseTree/NodeCondition/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-ParseTree-Cy',
-    version='1.0.4',
+    version='1.0.5',
     packages=['ParseTree', 'ParseTree.NodeCondition'],
     package_data={'ParseTree': ['*.pxd', '*.pyx', '*.c', '*.py'],
                   'ParseTree.NodeCondition': ['*.pxd', '*.pyx', '*.c', '*.py']},
@@ -14,5 +18,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Constituency Parse Tree Library',
-    install_requires = ['NlpToolkit-Dictionary-Cy']
+    install_requires = ['NlpToolkit-Dictionary-Cy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
