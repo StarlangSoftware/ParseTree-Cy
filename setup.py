58c9d47beae485042bdcca7e76893d566ac7111a
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-22 21:57:03 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["ParseTree/*.pyx", "ParseTree/NodeCondition/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-ParseTree-Cy',
-    version='1.0.7',
+    version='1.0.8',
     packages=['ParseTree', 'ParseTree.NodeCondition'],
     package_data={'ParseTree': ['*.pxd', '*.pyx', '*.c', '*.py'],
                   'ParseTree.NodeCondition': ['*.pxd', '*.pyx', '*.c', '*.py']},
