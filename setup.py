a2948cd827bbc539559147398d9ee05dac49edb7
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2025-04-01 21:30:31 +0300

    Updated name and version.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(["ParseTree/*.pyx", "ParseTree/NodeCondition/*.pyx"],
                           compiler_directives={'language_level': "3"}),
-    name='NlpToolkit-ParseTree-Cy',
-    version='1.0.10',
+    name='nlptoolkit-parsetree-cy',
+    version='1.0.11',
     packages=['ParseTree', 'ParseTree.NodeCondition'],
     package_data={'ParseTree': ['*.pxd', '*.pyx', '*.c', '*.py'],
                   'ParseTree.NodeCondition': ['*.pxd', '*.pyx', '*.c', '*.py']},
